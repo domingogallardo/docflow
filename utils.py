24489f91a0a79ac6effca616d5e5ec74fd6803d6
Domingo Gallardo <domingo.gallardo@ua.es>
2025-11-24 11:07:56 +0100

    fix(utils): normaliza <br> al añadir márgenes

--- utils.py
+++ utils.py
@@ -234,9 +234,12 @@ def add_margins_to_html_files(directory: Path, file_filter=None):
                     style_tag = soup.new_tag("style")
                     style_tag.string = margin_style + "\n" + img_rule
                     head.append(style_tag)
-            
+
+            output_html = str(soup)
+            # Normaliza saltos de línea para mantener <br> sin autocierre
+            output_html = output_html.replace("<br/>", "<br>").replace("<br />", "<br>")
             with open(html_file, 'w', encoding='utf-8') as f:
-                f.write(str(soup))
+                f.write(output_html)
             print(f"📏 Márgenes añadidos: {html_file.name}")
             
         except Exception as e:
